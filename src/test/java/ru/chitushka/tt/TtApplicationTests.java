d17783983008f5c5e17d538f0b4b280af858218d
Michael Jordon <jorik-super1@mail.ru>
2023-02-02 20:58:33 +0300

    Init Spring Boot tt project

---
+++ src/test/java/ru/chitushka/tt/TtApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chitushka.tt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
